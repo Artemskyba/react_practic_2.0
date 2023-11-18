9fb539d71bff3a098f581a0d909ca14bea79db12
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-11-18 13:17:52 +0200

    Modal

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import { MyModal } from 'components/MyModal/MyModal';
 
 export class Gallery extends Component {
   state = {
@@ -11,6 +12,8 @@ export class Gallery extends Component {
     error: null,
     isEmpty: false,
     isVisible: false,
+    showModal: false,
+    img: '',
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -25,35 +28,84 @@ export class Gallery extends Component {
     if (!query) {
       return;
     }
-    this.setState({ isLoading: true })
+    this.setState({ isLoading: true });
 
     try {
-      const { photos, total_results, page: currentPage, per_page } = await ImageService.getImages(query, page);
-      
+      const {
+        photos,
+        total_results,
+        page: currentPage,
+        per_page,
+      } = await ImageService.getImages(query, page);
+
       if (photos.length === 0) {
         this.setState({ isEmpty: true });
-      } 
+      }
 
-      this.setState(prevState => ({ images: [...prevState.images, ...photos], isVisible: currentPage < Math.ceil(total_results / per_page) }));
-
+      this.setState(prevState => ({
+        images: [...prevState.images, ...photos],
+        isVisible: currentPage < Math.ceil(total_results / per_page),
+      }));
     } catch (error) {
       this.setState({ error });
-
-    } finally { 
+    } finally {
       this.setState({ isLoading: false });
     }
-  }
-
-  onHandleSubmit = value => {
-    this.setState({ query: value });
   };
 
+  onHandleSubmit = value => {
+    this.setState({
+      query: value,
+      images: [],
+      page: 1,
+      isEmpty: false,
+      error: null,
+    });
+  };
+
+  onLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  openModal = img => {
+    this.setState({ showModal: true, img });
+  };
+
+  closeModal = () => {
+    this.setState({ showModal: false, img: '' });
+  };
   render() {
-    const { query } = this.state;
+    const { images, isVisible, isLoading, isEmpty, error, showModal, img } =
+      this.state;
     return (
       <>
         <SearchForm onSubmit={this.onHandleSubmit} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {error && (
+          <Text textAlign="center">❌ Something went wrong - {error}</Text>
+        )}
+        {isEmpty && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+        <Grid>
+          {images.length > 0 &&
+            images.map(({ id, avg_color, alt, src }) => (
+              <GridItem key={id} onClick={() => this.openModal(src.large)}>
+                <CardItem color={avg_color}>
+                  <img src={src.large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            ))}
+        </Grid>
+        <MyModal
+          modalIsOpen={showModal}
+          closeModal={this.closeModal}
+          src={img}
+        />
+        {isVisible && (
+          <Button onClick={this.onLoadMore} disabled={isLoading}>
+            {isLoading ? 'Loading...' : 'Load more'}
+          </Button>
+        )}
       </>
     );
   }
