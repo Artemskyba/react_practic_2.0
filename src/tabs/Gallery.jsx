2ce74810f78d734e22fecfd3d69bf10a1a7cd574
OlenaKovalenko <elenkow17@gmail.com>
2023-11-18 12:37:14 +0200

    Update Gallery.jsx

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -7,7 +7,42 @@ export class Gallery extends Component {
     query: '',
     page: 1,
     images: [],
+    isLoading: false,
+    error: null,
+    isEmpty: false,
+    isVisible: false,
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getPhotos(query, page);
+    }
+  }
+
+  getPhotos = async (query, page) => {
+    if (!query) {
+      return;
+    }
+    this.setState({ isLoading: true })
+
+    try {
+      const { photos, total_results, page: currentPage, per_page } = await ImageService.getImages(query, page);
+      
+      if (photos.length === 0) {
+        this.setState({ isEmpty: true });
+      } 
+
+      this.setState(prevState => ({ images: [...prevState.images, ...photos], isVisible: currentPage < Math.ceil(total_results / per_page) }));
+
+    } catch (error) {
+      this.setState({ error });
+
+    } finally { 
+      this.setState({ isLoading: false });
+    }
+  }
 
   onHandleSubmit = value => {
     this.setState({ query: value });
